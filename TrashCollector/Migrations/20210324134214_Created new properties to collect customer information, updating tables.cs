a7ec89b1d2af9ffbff717781111edfbbdece53bb
Denis Zavertyayev <denisz2277@gmail.com>
2021-03-24 07:57:47 -0700

    Updated the customer model to collect additional data and re-created the customer create view to reflect the additional data to be collected from the customer.

---
+++ TrashCollector/Migrations/20210324134214_Created new properties to collect customer information, updating tables.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrashCollector.Migrations
+{
+    public partial class Creatednewpropertiestocollectcustomerinformationupdatingtables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "44279a0a-d617-41b6-b33f-fba5c24476bb");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a96390c0-f93d-48b5-aae2-94fb4ccd4288");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Customers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Customers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Customers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ZipCode",
+                table: "Customers",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "7fc7661e-b786-48cb-aaec-c05b7038dffd", "14d5e8eb-f4de-41ae-8ac6-da473b8236a9", "Employee", "EMPLOYEE" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "ba99cd59-d42c-4518-8a06-f0e315603f89", "7246f339-4a86-4546-a0f1-a10ad4c0ccb3", "Customer", "CUSTOMER" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7fc7661e-b786-48cb-aaec-c05b7038dffd");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ba99cd59-d42c-4518-8a06-f0e315603f89");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                table: "Customers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "44279a0a-d617-41b6-b33f-fba5c24476bb", "c8f32fad-7586-4ad9-9f2e-9719819af8d1", "Employee", "EMPLOYEE" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "a96390c0-f93d-48b5-aae2-94fb4ccd4288", "ba4c38c4-145d-45f6-ab77-24124970d995", "Customer", "CUSTOMER" });
+        }
+    }
+}
--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -13,6 +13,11 @@ namespace TrashCollector.Models
         [Key]
         public int Id { get; set; }
         public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+
 
 
         [ForeignKey("IdentityUser")]
